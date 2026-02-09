4d9dd075075ea98fa1db5a003fb4aea555b726c9
Vikash <vikashyadav5131@gmail.com>
2026-02-09 17:12:17 +0530

    update middlewares

--- src/middlewares/video.middlewares.js
+++ src/middlewares/video.middlewares.js
@@ -1,31 +1,7 @@
 import ApiError from "../utils/ApiError.js";
-
 import asyncHandler from "../utils/asyncHandler.js";
-
 import { Video } from "../models/video.models.js";
-
 import { Channel } from "../models/channel.models.js";
-
-
-const loadVideoByUrl = asyncHandler(async (req, res, next) => {
-  const videoUrl = req.body?.videoUrl || req.params?.videoUrl;
-
-  if (!videoUrl) {
-    throw new ApiError(400, "videoUrl is required");
-  }
-
-  const video = await Video.findOne({ videoUrl });
-
-  if (!video) {
-    throw new ApiError(404, "Video not found", { videoUrl });
-  }
-
-
-  req.video = video;
-
-  next();
-
-});
 
 
 const verifyVideoOwner = asyncHandler(async (req, res, next) => {
@@ -45,7 +21,7 @@ const verifyVideoOwner = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, "Unauthorized");
   }
 
-  if (channel.owner.toString() !== req.user._id.toString()) {
+  if (channel.owner.toString() !== req.user?._id.toString()) {
     throw new ApiError(403, "You are not the owner of this video");
   }
 
@@ -54,7 +30,6 @@ const verifyVideoOwner = asyncHandler(async (req, res, next) => {
 
 
 export {
-  loadVideoByUrl,
   verifyVideoOwner,
 }
 
